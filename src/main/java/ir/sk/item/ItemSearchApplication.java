dd4072a468492f5c06c454f0e49f4f46fb19aa7f
Saeed kayvanfar <kayvanfar.sj@gamil.com>
2021-12-31 20:00:42 +0330

    project base is created.

---
+++ src/main/java/ir/sk/item/ItemSearchApplication.java
@@ -0,0 +1,14 @@
+package ir.sk.item;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by sad.kayvanfar on 12/31/2021
+ */
+@SpringBootApplication
+public class ItemSearchApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ItemSearchApplication.class, args);
+    }
+}
---
+++ src/test/java/ir/sk/item/ItemSearchApplicationTest.java
@@ -0,0 +1,18 @@
+package ir.sk.item;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ * Created by sad.kayvanfar on 12/31/2021
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ItemSearchApplicationTest {
+
+    @Test
+    public void contextLoads() {
+    }
+}
