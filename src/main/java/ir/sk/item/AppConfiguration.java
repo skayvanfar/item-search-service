0554833fa6331912399793028a26d0cad23828bb
Saeed kayvanfar <kayvanfar.sj@gamil.com>
2022-01-01 17:37:48 +0330

    fixes

---
+++ src/main/java/ir/sk/item/AppConfiguration.java
@@ -0,0 +1,37 @@
+package ir.sk.item;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
+import org.springframework.http.client.BufferingClientHttpRequestFactory;
+import org.springframework.http.client.ClientHttpRequestFactory;
+import org.springframework.http.client.SimpleClientHttpRequestFactory;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
+
+/**
+ * Created by sad.kayvanfar on 1/1/2022
+ */
+@Configuration
+@RequiredArgsConstructor
+public class AppConfiguration {
+
+    private final RestTemplateBuilder restTemplateBuilder;
+
+    @Bean
+    public RestTemplate restTemplate() {
+        ClientHttpRequestFactory factory = new BufferingClientHttpRequestFactory(new SimpleClientHttpRequestFactory());
+        RestTemplate restTemplate = restTemplateBuilder.build();
+        restTemplate.setRequestFactory(factory);
+        MappingJackson2HttpMessageConverter mappingJackson2HttpMessageConverter = new MappingJackson2HttpMessageConverter();
+        mappingJackson2HttpMessageConverter.setSupportedMediaTypes(
+                List.of(MediaType.valueOf("text/javascript;charset=utf-8"),
+                        MediaType.parseMediaType("application/json;charset=UTF-8")));
+        restTemplate.getMessageConverters().add(mappingJackson2HttpMessageConverter);
+        return restTemplate;
+    }
+}
